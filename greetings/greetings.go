a2613828d00fa0ddeb4cd1829b58bd2b245ed711
takumines <takumines1@gmail.com>
2024-11-30 13:04:20 +0900

    メッセージのフォーマットがランダムに変わるよう変更

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,17 +1,28 @@
 package greetings
 
 import (
-	"errors"
 	"fmt"
+	"math/rand"
 )
 
 // Hello 引数に受け取った名前を使ってメッセージを作成する
 func Hello(name string) (string, error) {
 	if name == "" {
-		return "", errors.New("empty name")
+		return "", fmt.Errorf("empty name")
 	}
 
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 
 	return message, nil
+}
+
+// ランダムなフォーマットのメッセージを返す
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
